d765cf812b29907f09ddb613fd53e72ff755cc3f
shiyi <shiyi@zugeliang01.com>
2020-12-06 01:24:23 +0800

    first commit1

--- view/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    url: '',
-    uploadUrl: 'http://newcrm.zugeliang01.com//api/upload',
-    uploadExcel: 'http://newcrm.zugeliang01.com//api/excel',
-    weChatBack: ''
-};
